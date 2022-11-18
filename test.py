137ff3416df0b318406c393d27fecf4f99c6d3e9
Daiki007121 <dkoike@mail.sfsu.edu>
2022-11-19 00:50:23 +0900

    ”Done”

---
+++ test.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import cv2
+
+
+original_face_img = "images/face_img.png"
+original_mask_img = "images/eye_img.png"
+
+
+cascade_file = "haarcascade_eye.xml"
+
+cascade = cv2.CascadeClassifier(cascade_file)
+
+face_img = cv2.imread(original_face_img)
+eye_img = cv2.imread(original_mask_img)
+
+
+face_list = cascade.detectMultiScale(face_img)
+
+def paste_img(n):
+    x = face_list[n][0]
+    y = face_list[n][1]
+    w = face_list[n][2]
+    h = face_list[n][3]
+
+    resized_eye_img = cv2.resize(eye_img, dsize=(w, h))
+    cv2.imwrite("images/resize_eye.png", resized_eye_img)
+    face_img[y : h + y, x : w + x] = cv2.imread("images/resize_eye.png")
+
+
+paste_img(0)
+paste_img(1)
+
+cv2.imwrite("images/face_eye.png", face_img)
